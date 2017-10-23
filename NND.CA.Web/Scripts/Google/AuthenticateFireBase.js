7aa608a82121be21251f5643e25c05ae8a0355d4
kian davoudi rad <davoudi.rk@gmail.com>
2017-10-23 09:45:47 -0700

    adding google sing sing on direct

--- NND.CA.Web/Scripts/Google/AuthenticateFireBase.js
+++ NND.CA.Web/Scripts/Google/AuthenticateFireBase.js
@@ -1,1 +1,44 @@
 ﻿"use strict";
+
+
+// Initialize Firebase
+/*
+Auhtor: Kian D.Rad
+Date: Oct 17th 2017
+ReadMe: Test file, to authenticate the firebase
+ToDO: Take out this to javascript file with propoer data models
+*/
+
+alert("here");
+function CallGoogleSinIn() {
+
+    var config = {
+        apiKey: "AIzaSyBRGDNklfhLgeYdMutMuOQnv3jqfDyCRj0",
+        authDomain: "nursenextdoor-ad735.firebaseapp.com",
+        databaseURL: "https://nursenextdoor-ad735.firebaseio.com",
+        projectId: "nursenextdoor-ad735",
+        storageBucket: "nursenextdoor-ad735.appspot.com",
+        messagingSenderId: "142428877752"
+    };
+
+    firebase.initializeApp(config);
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithRedirect(provider);
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().languageCode = 'el';
+    firebase.auth().signInWithRedirect(provider);
+};
+
+
+$(document).ready(function () {
+    // Handler for .ready() called.
+
+    $("#googleLogin").click(function () {
+        debugger;
+        CallGoogleSinIn();
+        alert("Handler for .click() called.");
+    });
+});
+
