053af7eae7eb9c27a8676946992415350fa8ae5e
kian davoudi rad <davoudi.rk@gmail.com>
2017-10-26 16:07:39 -0700

    I fixed the knockout bindle configuration, you can use this to assess the AuthenticateFireBase.js and call back to controller, which are test items, to return a list object where identifies where and how ViewModel will connect controller to the cshtml.

--- NND.CA.Web/App_Start/BundleConfig.cs
+++ NND.CA.Web/App_Start/BundleConfig.cs
@@ -45,10 +45,22 @@ namespace NND.CA.Web
                "~/Scripts/DefaultMvc/jquery-{version}.js",
                 "~/Scripts/Kendo/kendo.all.min.js",
                 "~/Scripts/Kendo/kendo.culture.en.min.js",
-                //"~/Scripts/Kendo/knockout-kendo.js",
                 "~/Scripts/Kendo/kendo.custom.min.js"
-                // "~/Scripts/Kendo/knockout-kendo.js"
-                // "~/Scripts/Kendo/knockout-kendo.js"
+
+                ));
+            #endregion
+
+            #region KnockOut
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+               // Add jQuery prior KendoUI 
+               "~/Scripts/DefaultMvc/jquery-{version}.js",
+                "~/Scripts/knockout/knockout.mapping-latest.debug.js",
+                "~/Scripts/knockout/knockout.mapping-latest.js",
+                "~/Scripts/knockout/knockout_341s.js",
+                "~/Scripts/knockout/knockout-2.1.0.debug.js",
+                "~/Scripts/knockout/knockout-3.0.0.debug.js",
+                "~/Scripts/knockout/knockout-3.4.2.js",
+                "~/Scripts/knockout/knockout-kendo.min.js"
 
                 ));
             #endregion
